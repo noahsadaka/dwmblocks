55c399b0ee05b4ac6cb47e807796cb8994cbf30e
Noah Sadaka <noahsadaka@gmail.com>
2021-01-16 22:08:39 -0500

    added cputemp, emojis, battery, Data disc space

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" 🌡️ ", "sensors | grep temp1: -m1 | awk '{print $2}' | sed 's/\+//'", 30, 0},
-	{" 💻 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" 🔋 ", "acpi | awk '{ print $4 }' | sed 's/,//'", 30, 0},
+	{"🖴 ", "~/.Scripts/datadisc", 30 , 0  },
+	{"🌡️ ", "sensors | grep temp1: -m1 | awk '{print $2}' | sed 's/\+//'", 20, 0},
+	{"💻 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	{"🕒 ", "date '+%b %d %I:%M%p'",					5,		0},
 };
 
